6b682347ade96e8f75f9d4bf8d2868eac6cc1fad
Jose Mamani <JOSEMLI@example.com>
2020-01-23 19:32:19 -0500

    se modifico la lectura de datos en vez del array  y que leea los datos del internet de tu base de datos y se conecto

--- app/src/main/java/com/example/android_u2_tema1/MainActivity.java
+++ app/src/main/java/com/example/android_u2_tema1/MainActivity.java
@@ -5,8 +5,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Vector;
 
@@ -26,11 +34,48 @@ public class MainActivity extends AppCompatActivity {
     misdatos = new ArrayList<>();
     misdatos.add(new Cliente("1", "Juanito", "Perez"));
     misdatos.add(new Cliente("2", "Pablito", "Canto"));
-    adaptador = new MiNuevoAdaptador(this, misdatos);
-/* adaptador = new MiNuevoAdaptador(this,
-ListaClientes());*/
+    //cambiamos esta linea que solo leia este array lkist por otra liena  que lee datos del internet
+   // adaptador = new MiNuevoAdaptador(this, misdatos);
+    //cambiamos por esta linea y se agrego el conseguirstring() en la lista cliente
+    adaptador = new MiNuevoAdaptador(this,
+      ListaClientes(conseguirstring()));
+
     recyclerView.setAdapter(adaptador);
     layoutManager = new LinearLayoutManager(this);
     recyclerView.setLayoutManager(layoutManager);
+  }
+
+  //Leyendo JSON
+  private ArrayList<Cliente> ListaClientes(String string) {
+    ArrayList<Cliente> Clientes = new ArrayList<>();
+    try {
+      JSONArray json_array = new JSONArray(string);
+      for (int i = 0; i < json_array.length(); i++) {
+        JSONObject objeto = json_array.getJSONObject(i);
+        Clientes.add(new Cliente(objeto.getString("Cod_persona"), objeto.getString("Nombre"),objeto.getString("Apellidos")));
+      }
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    return Clientes;
+  }
+  public String conseguirstring() {
+    try {
+      String miurl= this.getString(R.string.dominio)+this.getString(R.string.vercliente);
+      URL url=new URL(miurl);
+      conexion = (HttpURLConnection) url.openConnection();
+      if (conexion.getResponseCode() == HttpURLConnection.HTTP_OK) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+        String linea = reader.readLine();
+        res=linea;
+      } else {
+        Log.e("mierror", conexion.getResponseMessage());
+      }
+    } catch (Exception e) {
+      return res="";
+    } finally {
+      if (conexion!=null) conexion.disconnect();
+    }
+    return res;
   }
 }
