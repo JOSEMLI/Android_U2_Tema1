e0e2e1c781a1f543eeda24f4b5ee86ef5c2531fd
Jose Mamani <JOSEMLI@example.com>
2020-01-30 09:27:31 -0500

    ultima modificacion de insertar cliente y creacion del .php insertar clientepost

--- app/src/main/java/com/example/android_u2_tema1/InsertarCliente.java
+++ app/src/main/java/com/example/android_u2_tema1/InsertarCliente.java
@@ -9,8 +9,14 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -38,39 +44,43 @@ public class InsertarCliente extends AppCompatActivity {
   }
 
   public void Insertar(View view){
+    //se modifico el anterior por estee
     try {
-      String miurl= this.getString(R.string.dominio)+this.getString(R.string.insertarcliente)
-          + "nombre="+ URLEncoder.encode(nombre.getText().toString(),"UTF-8") + "&apellido="
-          + URLEncoder.encode(apellido.getText().toString(), "UTF-8") + "&sexo=" + isexo+"&telefono="
-          + nombre.getText().toString()+ "&telefono="+ URLEncoder.encode(telefono.getText().toString(), "UTF-8")
-          + "&direccion="+ URLEncoder.encode(direccion.getText().toString(), "UTF-8");
-      URL url=new URL(miurl);
-      Log.e("mierror",miurl);
-      conexion = (HttpURLConnection) url
-          .openConnection();
+      JSONObject postData = new JSONObject();
+      postData.put("nombre", nombre.getText().toString());
+      postData.put("apellido", apellido.getText().toString());
+      postData.put("sexo", isexo);
+      postData.put("telefono", telefono.getText().toString());
+      postData.put("direccion", direccion.getText().toString());
+      String myurl= this.getString(R.string.dominio)+this.getString(R.string.insertarclientepost);
+      Log.i("respuesta",myurl);
+      URL url = new URL(myurl);
+      conexion = (HttpURLConnection) url.openConnection();
+      conexion.setRequestProperty("Content-Type", "application/json");
+      conexion.setRequestMethod("POST");
+      conexion.setDoOutput(true);
+      conexion.setDoInput(true);
+      conexion.setChunkedStreamingMode(0);
+      OutputStream out = new BufferedOutputStream(conexion.getOutputStream());
+      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+          out, "UTF-8"));
+      writer.write(postData.toString());
+      writer.flush();
       if (conexion.getResponseCode() == HttpURLConnection.HTTP_OK) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
         String linea = reader.readLine();
-        if (!linea.equals("OK\\n")) {
-          Log.e("mierror","Error en servicio Web nueva");
-        }
+        if (!linea.equals("OK\\n")) Log.e("mierror","Error en servicio Web nueva");
         else
-        {
-          //este es un mensaje que le mistrara  con el toast
-          Toast.makeText(this, "Ingreso Exitosa", Toast.LENGTH_SHORT).show();
-
-          //y finalisamos la actividad y actualice
+        { Toast.makeText(this, "Inserción Exitosa", Toast.LENGTH_SHORT).show();
           finish();
-
-          Log.e("mierror","No hay error");
-        }
-      } else {
-        Log.e("mierror", conexion.getResponseMessage());
-      }
+          Log.e("mierror","No hay error");}
+      } else {Log.e("mierror", conexion.getResponseMessage());}
     } catch (Exception e) {
-      Log.e("mierror", e.getMessage(), e);
+      e.printStackTrace();
     } finally {
-      if (conexion!=null) conexion.disconnect();
+      if (conexion != null) {
+        conexion.disconnect();
+      }
     }
   }
 
